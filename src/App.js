01115b20c6c22ab4c6d0cd9eaed9f1e3a6ab07fc
Idkwhatnameisfree <giuliano.wendt@gmail.com>
2019-04-10 11:06:19 +0200

    hi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
+import Clock from './components/clock'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div className="App App-header">
+        <Clock />
       </div>
     );
   }
