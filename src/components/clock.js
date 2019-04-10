a57edd0da5da907e5473c397e6031797e8d11a0a
Idkwhatnameisfree <giuliano.wendt@gmail.com>
2019-04-10 10:33:16 +0200

    start

---
+++ src/components/clock.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class Clock extends Components {
+  render() {
+    return(
+      
+    )
+  }
+}
+
+export default Clock
